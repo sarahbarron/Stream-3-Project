09ca467a4ea82ac87ee085c9f080fe528dcf463b
Sarah Barron <sarahflavin@yahoo.com>
2018-05-24 16:49:25 +0000

    user messages activated

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 # Create your views here.
 
+# logout customer
 def logout_customer(request):
     
-    # when a customer selects logout
     auth.logout(request)
+    messages.success(request, "You have logged out successfully. We hope to see you again soon!")
     return redirect(reverse('get_index'))
+
+# def login_customer(request):
+    
+
+
+# def register_customer(request):
+
+
+
+# def customer_profile(request):
+
