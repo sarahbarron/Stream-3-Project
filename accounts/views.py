c7ee0f10e4d1ffb731d86f57d161033c802bf3d8
Sarah Barron <sarahflavin@yahoo.com>
2018-05-25 14:36:40 +0000

    working login form

--- accounts/views.py
+++ accounts/views.py
@@ -5,18 +5,22 @@ from .forms import CustomerLoginForm
 
 # logout customer returns the index page
 def logout_customer(request):
-    #logout the user
+    #logout the customer
     auth.logout(request)
-    #message that the Customer will see
+    
+    # the logout message that the customer will see
     messages.success(request, "You have logged out successfully. We hope to see you again soon!")
+    
+    #return to the home page index.html
     return redirect(reverse('get_index'))
 
 # login an already registered customer 
 def login_customer(request):
     
-    # if it = POST create an instance of the Customer login form and pass in the
-    # details that the customer has inputted into the form
+    # if its a POST method
     if request.method=="POST":
+        
+        #pass the details that the customer has inputted into for username and password
         customer_login_form = CustomerLoginForm(request.POST)
         
         # check to see if the data in the customer login form is valid
@@ -27,22 +31,20 @@ def login_customer(request):
             #if the form is valid
             if customer:
                 auth.login(user=customer, request=request)
-                messages.success(request, 'Your have successfully logged in. Welcome back!')
+                messages.success(request, 'You have successfully logged in. Welcome back!')
                 
             #otherwise send a message to say the form is invalid
             else:
                 customer_login_form.add_error(None, "Your username or password is incorrect, please try again!")
                 
-    #otherwise return an empty instance of the customer login form
-    
+    #If there is no POST method return an empty instance of the customer login form
     else:
         #create an instance of the login form
         customer_login_form = CustomerLoginForm()
     
-    
+    # go to the login.html file and input a customer_login_form
     return render(request, 'login.html', {"customer_login_form": customer_login_form})
-
-        
+    
     
     
 
