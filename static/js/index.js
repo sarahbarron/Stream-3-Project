3dd6903267fd6f1b49d0529e25519a5712bd7968
Sarah Barron <sarahflavin@yahoo.com>
2018-08-27 09:53:17 +0000

    changes to treatment flip cards to make hover affect more smooth

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,29 @@
+// CONTACT FORM - EMAIL.JS
+function sendMail(contactForm) {
+    emailjs.send("gmail", "ginas_beauty_studio", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "telephone": contactForm.telephone.value,
+        "comment": contactForm.comment.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS",response.status, response.text);
+            contactForm.reset();
+            text = 'Your message has been sent I will be in touch with you as soon as possible, Thank You Gina.';
+            document.getElementById("after-submit").innerHTML = text;
+        },
+        function(error) {
+            console.log("FAILED", error);
+            text= 'There seems to be a problem sending your email, please try again shortly!'
+             document.getElementById("after-submit").innerHTML = text;
+        }
+        
+    );
+    return false;  // To block from loading a new page
+}
 
-
+// NEWS & SPECIAL OFFERS CAROUSEL
 $(document).ready(function() {
     
     // news and special offers carousel on index.html page 
@@ -18,3 +42,12 @@ function initMap() {
   // The marker, positioned at Gina's Beauty
   var marker = new google.maps.Marker({position: ginas, map: map});
 }
+
+// TREATMENTS FLIP CARDS
+$(".flip").mouseenter(function(event){
+  $(this).addClass("mousein");
+});
+
+$(".flip").mouseleave(function(event){
+  $(this).removeClass("mousein");
+});
